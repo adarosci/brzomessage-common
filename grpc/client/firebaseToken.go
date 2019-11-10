dcb2e3bafe9b5555e6b927c7016411e5c40f3c10
Alisson Darosci <alisson_darosci@hotmail.com>
2019-11-10 16:48:40 -0300

    version

--- grpc/client/firebaseToken.go
+++ grpc/client/firebaseToken.go
@@ -9,7 +9,8 @@ import (
 	"google.golang.org/grpc"
 )
 
-var conn *grpc.ClientConn
+var connAPI *grpc.ClientConn
+var connPaypal *grpc.ClientConn
 
 // FirebaseTokenCommunication communication
 type FirebaseTokenCommunication struct{}
@@ -17,24 +18,42 @@ type FirebaseTokenCommunication struct{}
 func init() {
 	var err error
 	go func() {
-		conn, err = grpc.Dial("localhost:50051", grpc.WithInsecure(), grpc.WithBlock())
+		connAPI, err = grpc.Dial("localhost:50051", grpc.WithInsecure(), grpc.WithBlock())
 		if err != nil {
-			fmt.Printf("did not connect: %v", err)
+			fmt.Printf("did not connect api: %v", err)
+		}
+	}()
+	go func() {
+		connPaypal, err = grpc.Dial("localhost:50052", grpc.WithInsecure(), grpc.WithBlock())
+		if err != nil {
+			fmt.Printf("did not connect paypal: %v", err)
 		}
 	}()
 }
 
 // UpdateToken update token server
 func (gc FirebaseTokenCommunication) UpdateToken(keyAccess, firebaseToken string, phones []string) error {
-	c := communication.NewFirebaseTokenClient(conn)
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-	param := &communication.UpdateFirebaseToken{
-		Phones:        phones,
-		KeyAccess:     keyAccess,
-		FirebaseToken: firebaseToken,
-	}
-	_, err := c.UpdateApi(ctx, param)
-	_, err = c.UpdatePaypal(ctx, param)
-	return err
+	go func() {
+		c := communication.NewFirebaseTokenClient(connAPI)
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+		param := &communication.UpdateFirebaseToken{
+			Phones:        phones,
+			KeyAccess:     keyAccess,
+			FirebaseToken: firebaseToken,
+		}
+		c.UpdateApi(ctx, param)
+	}()
+	go func() {
+		c := communication.NewFirebaseTokenClient(connPaypal)
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+		param := &communication.UpdateFirebaseToken{
+			Phones:        phones,
+			KeyAccess:     keyAccess,
+			FirebaseToken: firebaseToken,
+		}
+		c.UpdatePaypal(ctx, param)
+	}()
+	return nil
 }
